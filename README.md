70acb24aad036641d595e6ccb2abd605c250506c
Trillexxx <61220798+Trillexxx@users.noreply.github.com>
2022-05-04 11:51:34 -0400

    knkn

--- README.md
+++
@@ -1,2 +0,0 @@
-# Once A-Pawn a Time
-A JAVA game developed for Game Programming Project by Shivam Persad and Brandon Noray
--- src/main/tiles/Tile.java
+++
@@ -1,9 +0,0 @@
-package main.tiles;
-
-import java.awt.image.BufferedImage;
-import java.awt.Graphics2D;
-
-public class Tile {
-    public BufferedImage image;
-    public boolean collision = false;
-}
